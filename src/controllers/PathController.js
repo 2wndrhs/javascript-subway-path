ddf23b2ec6710e5059f7e8eb9a0a5ff0f44686fd
2wndrhs <2wndrhs@gmail.com>
2022-12-14 14:19:12 +0900

    feat(subway): 경로 기준 기능을 입력받는 기능 구현

--- src/controllers/PathController.js
+++ src/controllers/PathController.js
@@ -1,9 +1,27 @@
 const InputView = require('../views/InputView');
 const OutputView = require('../views/OutputView');
 
+const { PATH } = require('../utils/constants');
+
 class PathController {
+  #featureHandlers = Object.freeze({
+    [PATH.DISTANCE]: '',
+    [PATH.TIME]: '',
+    [PATH.BACK]: '',
+  });
+
   start() {
     OutputView.printPathFeatures();
+
+    this.#inputPathFeature();
+  }
+
+  #inputPathFeature() {
+    InputView.readFeature(this.#onInputPathFeature.bind(this));
+  }
+
+  #onInputPathFeature(feature) {
+    this.#featureHandlers[feature]();
   }
 }
 
